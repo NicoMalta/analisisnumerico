e8aedd1c66d93daa295b51c25d82bccb6788d560
ucsec04 <niicou96@hotmail.com>
2017-08-07 21:52:33 -0300

    Primero

---
+++ ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class ResultadoRaiz
+    {
+        public int valorRaiz { get; set; }
+        public int iteraciones { get; set; }
+        public int error { get; set; }
+    }
+
+    public class Raices
+    {
+        public ResultadoRaiz Biseccion(int xi, int xd)
+        {
+            ResultadoRaiz nuevoResultado = new ResultadoRaiz();
+            int contador = 0;
+            bool negativo = true;
+            bool positivo = true;
+            if (nuevoResultado.iteraciones >= contador)
+            {
+                double puntoMedio = (xi + xd) / 2;
+                if ((funcion(xd) * funcion(xi)) < 0)
+                {
+
+                }
+            }
+            // Math.Pow(xi, 2);
+            return nuevoResultado;
+        }
+
+        public double funcion(int x)
+        {
+            double resultado = 0;
+            return resultado;
+        }
+
+        public double funcion(int x, int y)
+        {
+            double resultado = 0;
+            return resultado;
+        }
+
+        public double funcion(int x, int y, int z)
+        {
+            double resultado = 0;
+            return resultado;
+        }
+    }
+}
