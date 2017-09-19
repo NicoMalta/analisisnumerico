ac4cce88af38d20c4ccbb70bb2220019b23d92cf
NicoMalta <niicou96@hotmail.com>
2017-09-19 17:15:16 -0300

    SistemaEcuaciones

--- ClassLibrary2/ClassLibrary2/SistemaEcuaciones.cs
+++ ClassLibrary2/ClassLibrary2/SistemaEcuaciones.cs
@@ -8,6 +8,40 @@ namespace ClassLibrary2
 {
     class SistemaEcuaciones
     {
-       
+        public double[,] pivoteo(double[,] matriz)
+        {
+
+        }
+
+       public double[,] Gaussj(double[,] matriz, int cEcuaciones)
+        {
+          int normalizada = 1;
+            int ceros = 1;
+            if (matriz[1,1] == 0)
+            {
+                matriz = pivoteo(matriz);
+            }
+            double aux;
+            bool band = false;
+            for (int i = 1; i < cEcuaciones; i++)
+            {
+                double[] Ecuacion = new double[cEcuaciones + 1];
+                for (int c = 0; c < cEcuaciones+1; c++)
+                {
+                    Ecuacion[c] = matriz[i, c];
+                }
+
+                for (int c = 0; c < cEcuaciones + 1; c++)
+                {
+                    Ecuacion[c] = Ecuacion[c] / matriz[i, ceros];
+                    Ecuacion[c] = Ecuacion[c] / matriz[i+1, ceros];
+                    matriz[i + 1, c] = matriz[i + 1, c] - Ecuacion[c];
+                }
+
+
+                band = false;
+            }
+         
+        }
     }
 }
