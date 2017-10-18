dda4f66f2ff4984bf8e9e8c6dad2a209418d7376
nicomalta <niicou96@hotmail.com>
2017-10-18 20:06:01 -0300

    Regresion Polinomial

--- ClassLibrary2/ClassLibrary2/Regresion.cs
+++ ClassLibrary2/ClassLibrary2/Regresion.cs
@@ -6,24 +6,126 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary2
 {
-    class Regresion
+    public class Regresion
     {
-        public List<double> ListaX { get; set; }
-        public List<double> ListaY { get; set; }
-        public int n { get; set; }
 
-        Regresion()
+
+        public List<double> RL_MinimosCuadrados(int n, List<double> ListaX, List<double> ListaY)
         {
-           ListaX = new List<double>();
-           ListaY = new List<double>();
+            double aux = 0;
+            int contador = 0;
+            var ListaResultados = new List<double>();
+            foreach (var x in ListaX)
+            {
+                aux = aux + (x * ListaY[contador]);
+                contador++;
+
+            }
+
+            double a1 = ((n * aux) - (ListaX.Sum() * ListaY.Sum()))
+                                    /
+                ((n * (ListaX.Select(x => Math.Pow(x, 2)).Sum())) - (Math.Pow(ListaX.Sum(), 2)));
+
+            double a0 = (ListaY.Sum() / n) - a1 * (ListaX.Sum() / n);
+            ListaResultados.Add(a0);
+            ListaResultados.Add(-a0 / a1);
+
+            return ListaResultados;
         }
 
-        public double RL_MinimosCuadrados()
+        private double[,] Calcoeficientes(List<double> ListaX, List<double> ListaY, int grado)
         {
+            List<double> listasuma = new List<double>();
+            List<double> independiente = new List<double>();
+            double suma;
+            int n = ListaX.Count();
+            double[,] matriz = new double[grado + 1, grado + 2];
+            for (int k = 0; k <= 2 * grado; k++)
+            {
+                suma = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    suma += Math.Pow(ListaX[i], k);
+                }
+                listasuma.Add(suma);                
+            }
+            for (int k = 0; k <= grado; k++)
+            {
+                suma = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    suma += Math.Pow(ListaX[i], k) * ListaY[i];
+                }
+                independiente.Add(suma);
+            }
+            for (int i = 0; i <= grado; i++)
+            {
+                for (int j = 0; j <= grado; j++)
+                {
+                    matriz[i, j] = listasuma[i + j];
+                }
+            }
+            for (int i = 0; i <= grado; i++)
+            {
+                matriz[i, (grado + 1)] = independiente[i];
+            }
 
-            double a1 = (n * (ListaX.Sum() * ListaY.Sum()) - (ListaX.Sum() * ListaY.Sum()))
-                                    /
-                 (n * (ListaX.Select(x => Math.Pow(x, 2)).Sum()) - Math.Pow(ListaX.Sum(), 2));
+            return matriz;
+        }
+
+        private double CalR(List<double> ListaX, List<double> ListaY, List<double> polinomio)
+        {
+            double Yprom = ListaY.Sum() / ListaY.Count();
+            double st = 0;
+            double aux = 0;
+            double sr = 0;
+            double r = 0;
+            foreach (var item in ListaY)
+            {
+                st = st + Math.Pow((item - Yprom), 2);
+            }
+            for (int i = 0; i < ListaX.Count; i++)
+            {
+                int potencia = 0;
+                foreach (var item in polinomio)
+                {
+                    aux = aux - item * Math.Pow(ListaX[i], potencia);
+                    potencia++;
+                }
+                sr = sr + Math.Pow((ListaY[i] + aux), 2);
+                aux = 0; 
+            }
+            r = Math.Sqrt((st - sr) / st) * 100;
+            return r;
+         
+        }
+
+        public List<double> RP_MinimoCuadrados(List<double> ListaX, List<double> ListaY, double error)
+        {
+            var Polinomio = new List<double>();
+            
+            double r = 0;
+            int grado = 1;
+            double[,] matriz = new double[grado + 1, grado + 2];
+            SistemaEcuaciones probando = new SistemaEcuaciones();
+            
+            while (r < error && grado < 10 )
+            {
+                matriz = new double[grado + 1, grado + 2];
+                Polinomio = new List<double>();
+                matriz = Calcoeficientes(ListaX,ListaY, grado);
+                matriz = probando.Gaussj(matriz, matriz.GetLength(0));
+                for (int i = 0; i <= grado; i++)
+                {
+                    Polinomio.Add(matriz[i, (grado + 1)]);
+                }
+                r = CalR(ListaX,ListaY, Polinomio);
+                grado++;
+            }
+
+
+           
+            return Polinomio;
 
         }
     }
--- ClassLibrary2/UnitTestProject1/TestAnalisis.cs
+++ ClassLibrary2/UnitTestProject1/TestAnalisis.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ClassLibrary2;
 using org.mariuszgromada.math.mxparser;
+using WpfApp1;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
@@ -19,6 +21,35 @@ namespace UnitTestProject1
             var metodos = new Metodos();
 
             metodos.Secante(parametro,f);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var arrayX = new double[6] { -1,0,2,3,5,6};
+            var arrayY = new double[6] { 6, 4, 1, 1, 2, 5 };
+            ClassLibrary2.Regresion regresion = new ClassLibrary2.Regresion();
+            var listax = new List<double>();
+            var listay = new List<double>();
+            listax.Add(-1);
+            listax.Add(0);
+            listax.Add(2);
+            listax.Add(3);
+            listax.Add(5);
+            listax.Add(6);
+            listay.Add(6);
+            listay.Add(4);
+            listay.Add(1);
+            listay.Add(1);
+            listay.Add(2);
+            listay.Add(5);
+
+            double error = 80;
+
+            regresion.RP_MinimoCuadrados(listax,listay,error);
+
+            
 
         }
     }
